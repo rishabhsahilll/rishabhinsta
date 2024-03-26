3837a4f5e77887f87142c852c281ac240c7be339
RISHABH SAHIL <164033987+rishabhsahilll@users.noreply.github.com>
2024-03-26 12:21:24 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-# Starting build process
-echo "BUILD START"
+# Virtual environment create karna
+python3.9 -m venv myenv
 
-# Installing dependencies
+# Virtual environment activate karna
+source myenv/bin/activate
+
+# Dependencies install karna
 python3.9 -m pip install -r requirements.txt 
 
-# Making migrations
+# Django install karna
+python3.9 -m pip install django
+
+# Migrations banana
 echo "Make Migration"
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput 
 
-# Collecting static files
+# Static files collect karna
 echo "Collect Static"
 python3.9 manage.py collectstatic --noinput 
+
+# Virtual environment deactivate karna
+deactivate
 
 echo "Build process completed successfully"
