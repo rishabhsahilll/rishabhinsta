b20f341c2ff0d709afde0dbde380b6446e00fb29
RISHABH SAHIL <164033987+rishabhsahilll@users.noreply.github.com>
2024-03-26 12:18:12 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,14 +6,6 @@ echo "BUILD START"
 # Installing dependencies
 python3.9 -m pip install -r requirements.txt 
 
-# Step 3: Update Dependencies
-python3.9 -m pip install --upgrade urllib3 cloudinary
-
-# Step 5: Check Dependencies
-python3.9 -m pip list | grep urllib3
-python3.9 -m pip list | grep cloudinary
-
-
 # Making migrations
 echo "Make Migration"
 python3.9 manage.py makemigrations --noinput
